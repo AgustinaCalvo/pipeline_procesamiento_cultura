f2fd1f011663ad7befbb6e50f5d99d08c0d78805
AgustinaCalvo <magus90@hotmail.com>
2022-07-19 11:18:41 -0300

    Agregado modulo main

--- pipeline/descarga.py
+++ pipeline/descarga.py
@@ -5,12 +5,6 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-DATOS_CULTURA_URL = {
-    "museos": "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/4207def0-2ff7-41d5-9095-d42ae8207a5d/download/museos_datosabiertos.csv",
-    "bibliotecas": "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv",
-    "cines": "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/392ce1a8-ef11-4776-b280-6f1c7fae16ae/download/cine.csv",
-}
-
 
 def descargar_archivos(datos_dict):
     # TODO: Agregar mensaje de descarga y error
@@ -32,8 +26,15 @@ def descargar_archivos(datos_dict):
     return dict_path_archivos
 
 
+# Testeo de funcionamiento de funcion
+
 if __name__ == "__main__":
     logging.info("Descargando archivos")
+    DATOS_CULTURA_URL = {
+        "museos": "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/4207def0-2ff7-41d5-9095-d42ae8207a5d/download/museos_datosabiertos.csv",
+        "bibliotecas": "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv",
+        "cines": "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/392ce1a8-ef11-4776-b280-6f1c7fae16ae/download/cine.csv",
+    }
     path_archivo = descargar_archivos(DATOS_CULTURA_URL)
 
     print(path_archivo)
